be260414c52b5fb252f024e767323b5615b37803
eugenexlin <eugenexlin@gmail.com>
2017-11-12 23:58:28 -0800

    changed layout when discovering portrait tablet mode was not going to work

--- app/src/main/java/com/djdenpa/baker/ui/fragments/StepDetailFragment.java
+++ app/src/main/java/com/djdenpa/baker/ui/fragments/StepDetailFragment.java
@@ -1,12 +1,16 @@
 package com.djdenpa.baker.ui.fragments;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.djdenpa.baker.R;
@@ -20,13 +24,32 @@ public class StepDetailFragment extends Fragment {
 
   private Context mContext;
   private TextView mErrorMessage;
-
+  private  FrameLayout mExoPlayerFrameLayout;
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-    View rootView = inflater.inflate(R.layout.fragment_step_detail, container, false);
+    final View rootView = inflater.inflate(R.layout.fragment_step_detail, container, false);
     mContext = getContext();
+
+
+    mExoPlayerFrameLayout = rootView.findViewById(R.id.fl_exo_player);
+    //this will try to adapt the height of the video player to aspect ratio of the used screen space.
+
+    rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        double fragmentWidth = (float) mExoPlayerFrameLayout.getWidth();
+        //set the dimensions to 16:9 regardless of screen size.
+        int heightToSet = (int)Math.floor(fragmentWidth/16.0*9.0);
+        mExoPlayerFrameLayout.getLayoutParams().height = heightToSet;
+        mExoPlayerFrameLayout.requestLayout();
+
+        rootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+      }
+    });
+
+
 
     return rootView;
   }
